f3447b1027ac71eed9b31f76fc13473b9b78170b
Talles <tallesandrade7@gmail.com>
2024-10-18 13:14:19 -0300

    Criado controller

---
+++ src/main/java/dev/talles/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.talles.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/hello-world")
+    public String boasVinda(){
+        return "Hello World";
+    }
+}
