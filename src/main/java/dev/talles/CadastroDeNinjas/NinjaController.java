822fc37bdfff6738e2223fad97d54472b93eb427
Talles <tallesandrade7@gmail.com>
2024-10-20 21:10:52 -0300

    Cria uma entidade no banco de dados a partir do NinjaModel

--- src/main/java/dev/talles/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/talles/CadastroDeNinjas/NinjaController.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/hello-world")
     public String boasVinda(){
         return "Hello World";
+
     }
 }
