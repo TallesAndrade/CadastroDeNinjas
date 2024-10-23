7505737499aa7b7aee0557655fd7bbc1d0be380d
Talles <tallesandrade7@gmail.com>
2024-10-23 13:06:00 -0300

    Criação missão

--- src/main/java/dev/talles/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/talles/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -1,2 +1,48 @@
-package dev.talles.CadastroDeNinjas.Missoes;public class MissaoModel {
+package dev.talles.CadastroDeNinjas.Missoes;
+
+import dev.talles.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "td_missoes")
+public class MissaoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private char rank;
+
+    public MissaoModel(Long id, String nome,char rank) {
+        this.id = id;
+        this.nome = nome;
+        this.rank= rank;
+    }
+
+    public MissaoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public char getRank() {
+        return rank;
+    }
+
+    public void setRank(char rank) {
+        this.rank = rank;
+    }
 }
