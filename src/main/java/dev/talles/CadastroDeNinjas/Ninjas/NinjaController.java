1599f4c92685cf43a09620f64d6b43da50e0f254
Talles <tallesandrade7@gmail.com>
2024-11-07 13:24:54 -0300

    feat:atualiza o controller com mock de dados

--- src/main/java/dev/talles/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/talles/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.talles.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,35 @@ public class NinjaController {
     public String boasVinda(){
         return "Hello World";
 
+    }
+
+    //Add Ninja(CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    //Mostar Ninjas (READ)
+    @GetMapping("/todos")
+    public String montarTodosNinjas(){
+        return "Ninjas";
+    }
+
+    //Mostar Ninjas ID (READ)
+    @GetMapping("/todosID")
+    public String ninjasPorId(){
+        return "Ninjas Por ID";
+    }
+
+    //Alterar dados ninja(UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaId(){
+        return "Alterar Ninja ID";
+    }
+
+    //Deletar ninja(DELETE)
+    @DeleteMapping("deletarId")
+    public String deletarNinjaId(){
+        return "Ninja Deletado Por ID";
     }
 }
