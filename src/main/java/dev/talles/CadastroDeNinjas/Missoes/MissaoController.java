54f34c3ade90ac5488e4c55d80d30e4ca8f4c063
Talles <tallesandrade7@gmail.com>
2024-11-07 13:39:47 -0300

    feat:cria rotas do controller com mock

--- src/main/java/dev/talles/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/talles/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,4 +1,32 @@
 package dev.talles.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissaoController {
+
+    //POST - mandar requisição pra criar missões
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "missao criada";
+    }
+
+    //GET - mandar requisição pra listar missões
+    @GetMapping("/listar")
+    public String mostrarMissoes(){
+        return "Missoes";
+    }
+
+    //PUT - mandar requisição pra alterar missões
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterad com sucesso";
+    }
+
+    //DELETE - mandar requisição pra deletar missões
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada";
+    }
 }
